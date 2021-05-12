488f719150b2b8028e1fed31aae9561fe3423d65
OlgaMurashko <olgamurashko149@gmail.com>
2021-05-12 13:02:32 +0300

    Renamed login component

---
+++ src/app/components/authentication/components/email-verification/email-verification.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Self } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthFacade } from 'auth/store';
+import { EmailVerificationService } from './email-verification.service';
+
+@Component({
+  selector: 'app-email-verification',
+  templateUrl: './email-verification.component.html',
+  styleUrls: ['./email-verification.component.scss'],
+  providers: [EmailVerificationService]
+})
+export class EmailVerificationComponent implements OnInit {
+  public emailControl = new FormControl(
+    '',
+    [Validators.required, Validators.email],
+    [this.emailVerificationService.getEmailExistValidator()]
+  );
+
+  constructor(
+    @Self() public emailVerificationService: EmailVerificationService,
+    public authFacade: AuthFacade
+  ) {}
+
+  ngOnInit(): void {
+    this.emailControl.valueChanges.subscribe(() => {
+      console.log('errors', this.emailControl.errors, this.emailControl);
+      console.log('invalid', this.emailControl.invalid);
+      console.log('touched', this.emailControl.touched);
+      console.log('status', this.emailControl.status);
+    });
+  }
+
+  public onSubmit(): void {
+    const email = this.emailControl.value;
+  }
+}
